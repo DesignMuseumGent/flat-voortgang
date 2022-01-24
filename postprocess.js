26fe0a6e4a6c920ef85af66f3bbf5efff0dbd879
reinout.bossuyt <reinout.bossuyt@gmail.com>
2022-01-24 15:11:56 +0100

    update

--- postprocess.js
+++ postprocess.js
@@ -2,17 +2,74 @@
 
 // Helper library written for useful postprocessing tasks with Flat Data
 // Has helper functions for manipulating csv, json, excel, zip, and image files
-import { readCSV, writeJSON } from 'https://deno.land/x/flat@0.0.11/mod.ts' 
+import { readCSV, writeJSON } from 'https://deno.land/x/flat@0.0.11/mod.ts'
 
 // Step 1: Read the downloaded_filename JSON
-const filename = Deno.args[0] // Same name as downloaded_filename `const filename = 'btc-price.json';`
+const filename = Deno.args[0]; // Same name as downloaded_filename `const filename = 'btc-price.json';`
 const csv = await readCSV(filename)
-console.log(csv)
+let list = []
+
+for(let r in csv) {
+  if(csv[r].objectnummer) {
+    list.push(csv[r])
+  }
+}
+
+for(let l in list) {
+  list[l]['status'] = {}
+  if(list[l]['basisregistratie (fysiek object te bekijken)'] == "ok") {
+    list[l]['status']['basisregistratie'] = 'groen';
+  } else {
+    list[l]['status']['basisregistratie'] = 'rood';
+  }
+
+  if(list[l]['staat te ontsluiten beeld op de R-schijf in groot formaat?'] == "ok") {
+    list[l]['status']['beeldOpRSchijf'] = 'groen';
+  } else {
+    list[l]['status']['beeldOpRSchijf'] = 'rood';
+  }
+
+  if(list[l]['inhoudelijke registratie koepel en deelnummers'] == "ok") {
+    list[l]['status']['inhoudelijkeRegistratie'] = 'groen';
+  } else {
+    list[l]['status']['inhoudelijkeRegistratie'] = 'rood';
+  }
+
+  if(list[l]['fotorechten'] == "geklaard") {
+    list[l]['status']['fotoRechten'] = 'groen';
+  } else {
+    list[l]['status']['fotoRechten'] = 'rood';
+  }
+
+  if(list[l]['toestemmingAuteursrecht'] == "geklaard" || list[l]['toestemmingAuteursrecht'] == "ok") {
+    list[l]['status']['auteursrechten'] = 'groen';
+  } else {
+    list[l]['status']['auteursrechten'] = 'rood';
+  }
+
+  if(list[l]['publicatie op coghent (via europeana)'].search("geklaard") || list[l]['publicatie op coghent (via europeana)'].search("ok") || list[l]['publicatie op coghent (via europeana)'].search("OK")) {
+    list[l]['status']['publicatieEuropeana'] = 'groen';
+  } else {
+    list[l]['status']['publicatieEuropeana'] = 'rood';
+  }
+
+  if(list[l]['publicatie op erfgoedinzicht'].search("geklaard") || list[l]['publicatie op erfgoedinzicht'].search("ok") || list[l]['publicatie op erfgoedinzicht'].search("OK")) {
+    list[l]['status']['publicatieEuropeana'] = 'groen';
+  } else {
+    list[l]['status']['publicatieEuropeana'] = 'rood';
+  }
+
+  let naam = list[l]['naam'];
+  let objectnummer = list[l]['objectnummer']
+  list[l] = {'naam': naam, 'objectnummer': objectnummer, 'status': list[l].status}
+
+}
+
 
 // Step 2: Filter specific data we want to keep and write to a new JSON file
-const currencyRates = {'foo': 'bar'}
+
 
 // Step 3. Write a new JSON file with our filtered data
-const newFilename = `voortgang-postprocessed.csv` // name of a new file to be saved
-await writeJSON(newFilename, currencyRates) // create a new JSON file with just the Bitcoin price
+const newFilename = `voortgang-postprocessed.json` // name of a new file to be saved
+await writeJSON(newFilename, list) // create a new JSON file with just the Bitcoin price
 console.log("Wrote a post process file")
