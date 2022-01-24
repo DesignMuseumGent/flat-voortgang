6adb55d9cc0ddf0e110cbeda11092c09d7ae0900
reinoutbossuyt <reinout.bossuyt@gmail.com>
2022-01-24 13:37:54 +0100

    Update postprocess.js

--- postprocess.js
+++ postprocess.js
@@ -6,8 +6,8 @@ import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.11/mod.ts'
 
 // Step 1: Read the downloaded_filename JSON
 const filename = Deno.args[0] // Same name as downloaded_filename `const filename = 'btc-price.json';`
-const json = await readJSON(filename)
-console.log(json)
+const csv = await readCSV(filename)
+console.log(csv)
 
 // Step 2: Filter specific data we want to keep and write to a new JSON file
 const currencyRates = Object.values(json.bpi); // convert property values into an array
@@ -17,6 +17,6 @@ const filteredCurrencyRates = currencyRates.map(rate => ({
 }));
 
 // Step 3. Write a new JSON file with our filtered data
-const newFilename = `btc-price-postprocessed.json` // name of a new file to be saved
+const newFilename = `voortgang-postprocessed.csv` // name of a new file to be saved
 await writeJSON(newFilename, filteredCurrencyRates) // create a new JSON file with just the Bitcoin price
 console.log("Wrote a post process file")
